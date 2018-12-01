6580cdc70473a96d0eda9ac007970fc5ff390c0b
rindual <45397086+rindual@users.noreply.github.com>
2018-12-01 15:49:03 +0700

    android_uas.sql
    database

--- android_uas.sql
+++ android_uas.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 01 Des 2018 pada 06.35
--- Versi Server: 5.6.21
--- PHP Version: 5.6.3
+-- Generation Time: Dec 01, 2018 at 09:44 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `android_uas`
@@ -23,11 +25,11 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
-CREATE TABLE IF NOT EXISTS `barang` (
-`id_barang` int(11) NOT NULL,
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
   `nama_barang` varchar(255) NOT NULL,
   `warna_barang` varchar(255) NOT NULL,
   `kategori_barang` varchar(255) NOT NULL,
@@ -41,11 +43,11 @@ CREATE TABLE IF NOT EXISTS `barang` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `detail_transaksi`
+-- Table structure for table `detail_transaksi`
 --
 
-CREATE TABLE IF NOT EXISTS `detail_transaksi` (
-`id_detail` int(11) NOT NULL,
+CREATE TABLE `detail_transaksi` (
+  `id_detail` int(11) NOT NULL,
   `id_transaksi` int(11) NOT NULL,
   `id_barang` int(11) NOT NULL,
   `jumlah_beli` int(50) NOT NULL
@@ -54,11 +56,11 @@ CREATE TABLE IF NOT EXISTS `detail_transaksi` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `ongkos_kirim`
+-- Table structure for table `ongkos_kirim`
 --
 
-CREATE TABLE IF NOT EXISTS `ongkos_kirim` (
-`id_ongkir` int(11) NOT NULL,
+CREATE TABLE `ongkos_kirim` (
+  `id_ongkir` int(11) NOT NULL,
   `kota` varchar(255) NOT NULL,
   `harga` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -66,11 +68,11 @@ CREATE TABLE IF NOT EXISTS `ongkos_kirim` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pembeli`
+-- Table structure for table `pembeli`
 --
 
-CREATE TABLE IF NOT EXISTS `pembeli` (
-`id_pembeli` int(11) NOT NULL,
+CREATE TABLE `pembeli` (
+  `id_pembeli` int(11) NOT NULL,
   `nama_pembeli` varchar(255) NOT NULL,
   `alamat` varchar(255) NOT NULL,
   `telpn` int(15) NOT NULL,
@@ -81,16 +83,16 @@ CREATE TABLE IF NOT EXISTS `pembeli` (
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Table structure for table `transaksi`
 --
 
-CREATE TABLE IF NOT EXISTS `transaksi` (
-`id_transaksi` int(11) NOT NULL,
+CREATE TABLE `transaksi` (
+  `id_transaksi` int(11) NOT NULL,
   `id_pembeli` int(11) NOT NULL,
   `id_ongkir` int(11) NOT NULL,
   `total_harga` int(50) NOT NULL,
   `tgl_beli` date NOT NULL,
-  `status` varchar(255) NOT NULL
+  `status` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -101,31 +103,35 @@ CREATE TABLE IF NOT EXISTS `transaksi` (
 -- Indexes for table `barang`
 --
 ALTER TABLE `barang`
- ADD PRIMARY KEY (`id_barang`);
+  ADD PRIMARY KEY (`id_barang`);
 
 --
 -- Indexes for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
- ADD PRIMARY KEY (`id_detail`), ADD KEY `id_transaksi` (`id_transaksi`), ADD KEY `id_barang` (`id_barang`);
+  ADD PRIMARY KEY (`id_detail`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_barang` (`id_barang`);
 
 --
 -- Indexes for table `ongkos_kirim`
 --
 ALTER TABLE `ongkos_kirim`
- ADD PRIMARY KEY (`id_ongkir`);
+  ADD PRIMARY KEY (`id_ongkir`);
 
 --
 -- Indexes for table `pembeli`
 --
 ALTER TABLE `pembeli`
- ADD PRIMARY KEY (`id_pembeli`);
+  ADD PRIMARY KEY (`id_pembeli`);
 
 --
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
- ADD PRIMARY KEY (`id_transaksi`), ADD KEY `id_pembeli` (`id_pembeli`), ADD KEY `id_ongkir` (`id_ongkir`);
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `id_pembeli` (`id_pembeli`),
+  ADD KEY `id_ongkir` (`id_ongkir`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -135,44 +141,50 @@ ALTER TABLE `transaksi`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `ongkos_kirim`
 --
 ALTER TABLE `ongkos_kirim`
-MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_ongkir` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `pembeli`
 --
 ALTER TABLE `pembeli`
-MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pembeli` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `detail_transaksi`
+-- Constraints for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id_transaksi`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id_barang`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `transaksi`
+-- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_ongkir`) REFERENCES `ongkos_kirim` (`id_ongkir`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id_pembeli`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_ongkir`) REFERENCES `ongkos_kirim` (`id_ongkir`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
